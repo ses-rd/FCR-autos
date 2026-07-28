56c87c86bd060b2ee590df645dde855c2fcb7ad0
Adrian Segura Trista <asegurat15@gmail.com>
2026-07-28 08:42:05 -0400

    add.

---
+++ fcr_invoice_report/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "nx_lcr_invoice_report",
+    "version": "19.0.1.0.0",
+    "author": "SEGU",
+    "category": "Accounting",
+    "summary": "Custom invoice",
+    "depends": ["l10n_do_ecf"],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+}
