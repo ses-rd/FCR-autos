26bcf8469672e0eb3204daaea84f02083bb8825a
Adrian Segura Trista <asegurat15@gmail.com>
2026-08-11 11:55:11 -0400

    add.

---
+++ fcr_sale_management/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "FCR - Sale Management",
+    "version": "19.0.1.0.0",
+    "category": "Sales/Sales",
+    "summary": """
+        Módulo para gestionar presupuestos, pedidos y plantillas.
+     """,
+    "author": "SEGU",
+    "depends": ["sale_management", "fcr_invoice_report"],
+    "data": [
+        "views/sale_order_report.xml",
+    ],
+    "license": "LGPL-3",
+}
