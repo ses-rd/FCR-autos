11bde4b0e8c5fd2f74682e3a307197c363cec2c6
Adrian Segura Trista <asegurat15@gmail.com>
2026-09-02 18:05:44 -0400

    manifest.

--- l10n_do_fcr_auto_invoice/__manifest__.py
+++ l10n_do_fcr_auto_invoice/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "FCR - Invoice Report",
     "version": "19.0.1.0.0",
     "category": "Localization",
-    "summary": "Fix layout of l10n_cr invoice report information block",
+    "summary": "Fix layout of l10n_do_ecf invoice report information block",
     "author": "SEGU",
     "license": "LGPL-3",
     "depends": [
