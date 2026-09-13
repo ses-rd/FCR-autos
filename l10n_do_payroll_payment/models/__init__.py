c2a6b8c33125c02ca4f1cbf43c33b03381a63cc1
Adrian Segura Trista <asegurat15@gmail.com>
2026-09-13 12:47:34 -0400

    del.

--- l10n_do_payroll_payment/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_journal
-from . import account_batch_payment
-from . import hr_payslip
-from . import hr_payslip_run
-from . import res_bank
-from . import res_partner_bank
--- l10n_do_payroll_payment/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .payroll_txt import generate_data_for_bank
