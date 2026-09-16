c63ac0e3c29407ec592a8167881fb6df7244885b
Joas Diaz <joasdiaz98@gmail.com>
2026-09-16 00:02:04 -0400

    feat: add vehicle outgoing conduce report

---
+++ fcr_vehicle_conduce/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'FCR - Conduce de vehículos',
+    'version': '19.0.1.0.0',
+    'summary': 'Conduce de salida desde entregas de vehículos vinculadas a ventas',
+    'author': 'SEGU',
+    'category': 'Inventory/Inventory',
+    'license': 'LGPL-3',
+    'depends': ['eg_fleet_product_link', 'sale_stock', 'web'],
+    'data': [
+        'report/vehicle_conduce_paperformat.xml',
+        'report/vehicle_conduce_templates.xml',
+        'report/vehicle_conduce_actions.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
